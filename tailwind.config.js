44fc4c3d875235b5a48c35a21123b4f36f37d000
Valton Salihu <valtonsalihu10@gmail.com>
2025-01-24 23:19:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', './**/*.js'], // Specify the paths to your HTML and JS files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
